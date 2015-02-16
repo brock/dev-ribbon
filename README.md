b9265ab098ab3d34aca8d3019d39de8da3bd50c2
Brock Angelo <brockangelo@gmail.com>
2015-02-15 20:25:19 -0600

    add scripts and update readme

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # dev-ribbon
 Add a ribbon to your development sites
+
+Add this tag to your HTML, and if the `window.location.hostname` matches `localhost`, `.dev` or `.local` it will show a green "Development" banner in the top right.
---
+++ dev-ribbon.js
@@ -0,0 +1,13 @@
+function devRibbon(str) {
+    switch (true) {
+      case /localhost/.test(str):
+      case /.*\.dev$/.test(str):
+      case /.*\.local$/.test(str):
+        document.body.innerHTML += "<div style='position:fixed;padding:5px 45px;width:225px;background-color:#087c15;color:#fff;font-size:13px;font-family:sans-serif;text-decoration:none; font-weight:bold;-webkit-backface-visibility:hidden;border:2px dotted #fff;box-shadow:0 0 0 3px #087c15,0 0 20px -3px rgba(0,0,0,.5);text-shadow:0 0 0 #fff,0 0 5px rgba(0,0,0,.3);top:50px;right:-50px;-ms-transform:rotate(45deg);-moz-transform:rotate(45deg);-webkit-transform:rotate(45deg);transform: rotate(45deg);'>Development</div>"
+        break;
+      default:
+        break;
+    }
+}
+
+devRibbon(window.location.hostname);
